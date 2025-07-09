4a5bc9df5a2c50dbb5370e02f3559c0eac20f138
HendriAzwar <maulanaazwarhendri@gmail.com>
2025-07-09 14:49:44 +0700

    Update railway1

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import graphRoutes from "./routes/graph.js";
 import roomRoutes from "./routes/room.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000; // ✅ penting untuk Railway / Render
 
 app.use(cors({
   origin: "https://golden-seahorse-ff992c.netlify.app" // ganti dengan domain netlifymu
@@ -17,6 +17,10 @@ app.use(express.json());
 // app.use('/api/auth', authRoutes);
 app.use("/api/grafik", graphRoutes);
 app.use("/api", roomRoutes);
+
+app.get("/", (req, res) => {
+  res.send("✅ Backend API is running successfully 🚀");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
